dca31448a7e87ea423f89f48ab14df5ec6e12fed
Maribel <mxc406@miami.edu>
2019-09-10 11:35:33 -0400

    Update sketch.js
    test

--- hw/attempts/sketch.js
+++ hw/attempts/sketch.js
@@ -9,7 +9,7 @@ function setup() {
   ellipse(500,500,100,100)
 
 strokeWeight(50);
-  line(300, 250, 700, 250);
+  line(500, 250, 700, 250);
 
   arc(750, 700, 200, 200, 0, QUARTER_PI);
 // put setup code here
