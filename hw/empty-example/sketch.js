2f3d870aed630bb4a86ce2825b7057bb689dc844
Maribel <mxc406@miami.edu>
2019-09-18 12:16:05 -0400

    moved to draw

--- hw/empty-example/sketch.js
+++ hw/empty-example/sketch.js
@@ -9,22 +9,24 @@ var eyeColor= "black";
 function setup() {
 
   createCanvas(1500,1000);
+
+
+// put setup code here
+}
+
+function draw() {
   background("blue");
 
+  fill("white");
   ellipse(X,Y,B,B)
 
   fill(eyeColor);
   ellipse(X,Y,100,100)
 
-strokeWeight(50);
+  strokeWeight(50);
   line(X-200, Y-250, 700, B);
 
   arc(X+250, Y+200, A, A, C, QUARTER_PI);
-// put setup code here
-}
-
-function draw() {
-
 }
 
 function keyPressed(){
