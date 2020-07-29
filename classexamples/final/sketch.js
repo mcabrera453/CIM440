e14c94a5433d53289f2e61ae4521a39314020a33
Maribel <mxc406@miami.edu>
2020-07-28 20:53:06 -0400

    final

--- classexamples/final/sketch.js
+++ classexamples/final/sketch.js
@@ -1,39 +1,93 @@
 // Sloth image "IMG_3883" by JohnHedtke is licensed under CC BY-NC-SA 2.0 https://search.creativecommons.org/photos/217faedd-d06a-48ba-91f7-f4d4c6dafa04
 // Puppy image "puppies" by www.petian.net is licensed under CC BY-NC-ND 2.0  https://search.creativecommons.org/photos/4f271583-e45d-4faf-bfc8-7291c7c1e3c3
 // dinosaur "Rupenhorn Dinosaur #4" by Kristian_Laban is licensed under CC BY-NC 2.0 https://search.creativecommons.org/photos/96213ba5-e9b6-4f2f-91e4-da28b9fcad22
-var title, puppy, dinosaur;
-var sButton, pButton, dButton;
+var zero, one, two, three, four, five, six, seven, eight, nine;
+var sButton, pButton;
 var currentImage = 0;
 
 function preload (){
-title = loadImage("images/igirltitle.png");
-puppy = loadImage("images/puppy.jpg");
-dinosaur = loadImage("images/dinosaur.jpg");
-
+zero = loadImage("images/zero.png");
+one = loadImage("images/one.png");
+two = loadImage("images/two.png");
+three = loadImage("images/three.png");
+four = loadImage("images/four.png");
+five = loadImage("images/five.png");
+six = loadImage("images/six.png");
+seven = loadImage("images/seven.png");
+eight = loadImage("images/eight.png");
+nine = loadImage("images/nine.png");
 }
 
 
 function setup() {
   // put setup code here
-createCanvas(400,400);
+createCanvas(720,400);
 
-sButton = createButton("title");
+sButton = createButton("yes");
 sButton.mousePressed(function(){
-currentImage = 0;
-
+//do you want to start?
+if (currentImage == 0){
+  currentImage = 1;
+} else if (currentImage == 1){
+  currentImage = 2;
+}
+else if (currentImage == 2){
+  currentImage = 5;
+}
+else if (currentImage == 5){
+  currentImage = 6;
+}
+else if (currentImage == 6){
+  currentImage = 7;
+}
+else if (currentImage == 7){
+  currentImage = 8;
+}
+else if (currentImage == 8){
+  currentImage = 9;
+}
+else if (currentImage == 9){
+  currentImage = 0;
+}
+else if (currentImage == 3){
+  currentImage = 5;
+}
+else if (currentImage == 4){
+  currentImage = 5;
+}
 });
 
-pButton = createButton("puppy");
+pButton = createButton("no");
 pButton.mousePressed(function(){
-currentImage = 1;
-
+  if (currentImage == 0){
+    currentImage = 0;
+  }
+  else if (currentImage == 1){
+    currentImage = 3;
+  }
+  else if (currentImage == 2){
+    currentImage = 4;
+  }
+  else if (currentImage == 3){
+    currentImage = 9;
+  }
+  else if (currentImage == 4){
+    currentImage = 5;
+  }
+  else if (currentImage == 5){
+    currentImage = 6;
+  }
+  else if (currentImage == 6){
+    currentImage = 7;
+  }
+  else if (currentImage == 7){
+    currentImage = 8;
+  }
+  else if (currentImage == 8){
+    currentImage = 9;
+  }
 });
 
-dButton = createButton("dinosaur");
-dButton.mousePressed(function(){
-currentImage = 2;
-
-});
 }
 
 function draw() {
@@ -41,13 +95,35 @@ function draw() {
   console.log("currentImage" + currentImage);
   // put drawing code here
 if(currentImage == 0){
-  image(title,0,0,title.width/4,title.height/4);
-} else if(currentImage == 1){
-  image(puppy,0,0,puppy.width/4,puppy.height/4);
-} else if(currentImage == 2){
-  image(dinosaur,0,0,dinosaur.width/4,dinosaur.height/4);
+  image(zero,0,0,zero.width/2,zero.height/2);
 }
 
-
+ else if(currentImage == 1){
+  image(one,0,0,one.width/2,one.height/2);
+}
+else if(currentImage == 2){
+  image(two,0,0,two.width/2,two.height/2);
+}
+else if(currentImage == 3){
+  image(three,0,0,three.width/2,three.height/2);
+}
+else if(currentImage == 4){
+  image(four,0,0,four.width/2,four.height/2);
+}
+else if(currentImage == 5){
+  image(five,0,0,five.width/2,five.height/2);
+}
+else if(currentImage == 6){
+  image(six,0,0,six.width/2,six.height/2);
+}
+else if(currentImage == 7){
+  image(seven,0,0,seven.width/2,seven.height/2);
+}
+else if(currentImage == 8){
+  image(eight,0,0,eight.width/2,eight.height/2);
+}
+else if(currentImage == 9){
+  image(nine,0,0,nine.width/2,nine.height/2);
+}
 
 }
